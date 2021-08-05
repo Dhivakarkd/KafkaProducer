cc0d1f951f30ebde5a27cd4536e82729971fca0b
Dhivakarkd <dhivakar084.github.com>
2021-08-05 20:06:26 +0530

    renamed package

--- src/main/java/com/dhivakar/kafkaproducer/KafkaProducerApplication.java
+++ src/main/java/com/dhivakar/kafkaproducer/KafkaProducerApplication.java
@@ -1,4 +1,4 @@
-package com.dhivakar.kafkaProducer;
+package com.dhivakar.kafkaproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dhivakar/kafkaproducer/KafkaProducerApplicationTests.java
+++ src/test/java/com/dhivakar/kafkaproducer/KafkaProducerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dhivakar.kafkaProducer;
+package com.dhivakar.kafkaproducer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
