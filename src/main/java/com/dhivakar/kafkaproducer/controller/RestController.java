4e3dc24bf877e23ded41bbaa035f62541cc294a7
Dhivakarkd <dhivakar084.github.com>
2021-08-10 22:58:23 +0530

    added KafkaProducer

--- src/main/java/com/dhivakar/kafkaproducer/controller/RestController.java
+++
@@ -1,4 +0,0 @@
-package com.dhivakar.kafkaproducer.controller;
-
-public class RestController {
-}
--- src/main/java/com/dhivakar/kafkaproducer/producer/KafkaProducer.java
+++ src/main/java/com/dhivakar/kafkaproducer/producer/KafkaProducer.java
@@ -2,7 +2,9 @@ package com.dhivakar.kafkaproducer.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KafkaProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
